4ccac71a990bf44b93bdf062cf52db574f3ff035
leshgun <nogalexey@gmail.com>
2022-10-10 16:23:44 +0200

    add loading icon and error handler

--- src/components/DocList.jsx
+++ src/components/DocList.jsx
@@ -1,14 +1,27 @@
-import "../styles/DocList.css"
-
 // import React, { useState } from 'react'
 import Doc from "./Doc"
+import MyLoading from "../UI/loading/MyLoading";
 
-function Docs({docs}) {
+import mprint from "../utils/myPrint";
+import "../styles/DocList.css"
+
+
+
+function Docs({docs=[], error}) {
+
+	const loading = <MyLoading />;
+	const defError = <h2>Loh...</h2>;
+
+	let children;
+	if (docs.length) children = docs.map(doc => <Doc doc={doc} key={doc.id} />);
+	else children = error || loading || defError;
 
 	return (
 		<div className="docs">
-			<h3 style={{display: 'flex', justifyContent: 'center'}}>Доки:</h3>
-			{docs.map(doc => <Doc doc={doc} key={doc.id} />)}
+			<h3 style={{display: 'flex', justifyContent: 'center'}}>
+				Репозитории пользователя:
+			</h3>
+			{children}
 		</div>
 	)
 }
